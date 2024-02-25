67ade1e4224e6cb3bd623e84f4f02f406fccc9b3
RealFaceCode <kevin-ernst@xyz.de>
2024-02-25 19:59:38 +0100

    Add 'f' directory and files to tests/CMakeLists.txt

---
+++ tests/f/file.cpp
@@ -0,0 +1,27 @@
+#include "file.hpp"
+#include "../test_util.hpp"
+#include "eutil.hpp"
+
+int main()
+{
+    eutil::Init();
+
+    eutil::File write("test.txt", eutil::FileOpenMode::Write);
+    write.open();
+    write.push_back(std::string("Hello, World!"), true);
+    write.write();
+    write.close();
+
+    eutil::File file("test.txt", eutil::FileOpenMode::Read);
+    file.open();
+    std::cout << "File size: " << file.size() << std::endl;
+    file.read();
+
+    auto first = file.toString();
+
+    ::compare("Hello, World!", first, "File read failed.");
+
+    file.close();
+
+    return ::FAILED;
+}
