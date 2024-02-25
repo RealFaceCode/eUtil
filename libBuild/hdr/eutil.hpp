c6d33b29eb56fd41dbf1bd51497728cc22b0ed08
RealFaceCode <kevin-ernst@xyz.de>
2024-02-25 19:59:46 +0100

    Add eutil.hpp and eutil.cpp files

---
+++ libBuild/hdr/eutil.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace eutil
+{
+    void Init();
+}
---
+++ libBuild/src/eutil.cpp
@@ -0,0 +1,10 @@
+#include "eutil.hpp"
+#include "elog.hpp"
+
+namespace eutil
+{
+    void Init()
+    {
+        elog::Init();
+    }
+}
