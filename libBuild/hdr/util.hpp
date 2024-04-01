1fcc2b7fd16401520253eb21ecf29b6132fcdf81
RealFaceCode <kevin-ernst@xyz.de>
2024-04-01 17:07:55 +0200

    Add getCurrentTimeDate function to util.hpp and util.cpp

--- libBuild/hdr/util.hpp
+++ libBuild/hdr/util.hpp
@@ -6,4 +6,5 @@
 namespace eutil
 {
     std::string getCurrentTime(std::string_view format = "%Y-%m-%d %H:%M:%S");
+    auto getCurrentTimeDate(std::string_view timeFormat = "%H:%M:%S", std::string_view dateFormat = "%Y-%m-%d");
 }
--- libBuild/src/util.cpp
+++ libBuild/src/util.cpp
@@ -13,4 +13,20 @@ namespace eutil
         
         return timeStringStream.str();
     }
+
+    auto getCurrentTimeDate(std::string_view timeFormat, std::string_view dateFormat)
+    {
+        auto now = std::chrono::system_clock::now();
+        auto now_c = std::chrono::system_clock::to_time_t(now);
+        
+        std::tm const* localTime = std::localtime(&now_c);
+        
+        std::stringstream timeStringStream;
+        timeStringStream << std::put_time(localTime, timeFormat.data());
+        
+        std::stringstream dateStringStream;
+        dateStringStream << std::put_time(localTime, dateFormat.data());
+        
+        return std::make_pair(timeStringStream.str(), dateStringStream.str());
+    }
 }
