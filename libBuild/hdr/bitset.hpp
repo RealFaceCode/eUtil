a8d0695bf52eba51c67ea31ae9c3c871d76409da
RealFaceCode <kevin-ernst@xyz.de>
2024-04-02 22:51:28 +0200

    Add EUTIL_API macro to Bitset class

--- libBuild/hdr/bitset.hpp
+++ libBuild/hdr/bitset.hpp
@@ -2,16 +2,18 @@
 
 #include <cstdint>
 
+#include "defines.hpp"
+
 namespace eutils
 {
-    struct Bitset
+    struct EUTIL_API Bitset
     {
     public:
-        Bitset();
-        explicit Bitset(uint32_t data);
+        EUTIL_API Bitset();
+        EUTIL_API explicit Bitset(uint32_t data);
 
-        void set(uint32_t index, bool bit);
-        bool get(uint32_t index) const;
+        EUTIL_API void set(uint32_t index, bool bit);
+        EUTIL_API bool get(uint32_t index) const;
 
     private:
         uint32_t m_data = 0;
--- libBuild/src/bitset.cpp
+++ libBuild/src/bitset.cpp
@@ -2,19 +2,19 @@
 
 namespace eutils
 {
-    Bitset::Bitset() = default;
+    EUTIL_API Bitset::Bitset() = default;
 
-    Bitset::Bitset(uint32_t data)
+    EUTIL_API Bitset::Bitset(uint32_t data)
         : m_data(data)
     {
     }
 
-    void Bitset::set(uint32_t index, bool bit)
+    EUTIL_API void Bitset::set(uint32_t index, bool bit)
     {
         bit ? m_data |= (1 << index) : m_data &= ~(1 << index);
     }
 
-    bool Bitset::get(uint32_t index) const
+    EUTIL_API bool Bitset::get(uint32_t index) const
     {
         return (m_data >> index) & 1;
     }
