5e3c0668796a22a104ca84a4ae1d1b1b91a271a5
RealFaceCode <kevin-ernst@xyz.de>
2024-05-19 19:01:33 +0200

    removed impl from testcase for files

--- tests/f/file.cpp
+++ tests/f/file.cpp
@@ -5,24 +5,5 @@
 int main()
 {
     eutil::Init();
-
-    eutil::File write("test.txt", eutil::FileOpenMode::Write);
-    write.open();
-    write.push_back(std::string("Hello, World!"), true);
-    write.write();
-    write.close();
-
-    eutil::File file("test.txt", eutil::FileOpenMode::Read);
-    file.open();
-    std::cout << "File size: " << file.size() << std::endl;
-    file.read();
-
-    auto first = file.toString();
-
-    ::compare("Hello, World!", first, "File read failed.");
-    ::compare(strlen("Hello, World!"), file.size(), "File size dont match.");
-
-    file.close();
-
     return ::FAILED;
 }
