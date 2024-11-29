5ef94b5635247445e2abd8b9aa90366da406eb61
RealFaceCode <kevin-ernst@xyz.de>
2024-11-29 17:39:29 +0100

    Add ID and IDPool classes for ID management in idp module

---
+++ libBuild/hdr/idp.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "defines.hpp"
+#include <queue>
+#include <deque>
+#include <memory>
+#include <cstdint>
+
+namespace eutil::idp
+{
+    class ID;
+
+    class EUTIL_API IDPool
+    {
+    public:
+        IDPool();
+        IDPool(uint64_t minID, uint64_t maxID);
+        ~IDPool();
+
+        ID getID();
+        void releaseID(uint64_t id);
+        void releaseID(ID id);
+
+        uint64_t getMinID() const;
+        uint64_t getMaxID() const;
+        uint64_t getAvailableIDs() const;
+
+    private:
+        void sortIDs();
+
+        uint64_t m_minID;
+        uint64_t m_maxID;
+        std::deque<uint64_t> idQueue;
+        std::vector<std::shared_ptr<uint64_t>> idList;
+    };
+
+    class EUTIL_API ID
+    {
+    public:
+        ID();
+        ID(std::shared_ptr<uint64_t> id, IDPool* pool);
+        ID(const ID& other);
+        ID(ID&& other);
+        ~ID();
+
+        uint64_t get() const;
+
+    private:
+        std::shared_ptr<uint64_t> id;
+        IDPool* pool;
+    };
+}
---
+++ libBuild/src/idp.cpp
@@ -0,0 +1,114 @@
+#include "idp.hpp"
+
+namespace eutil::idp
+{
+    IDPool::IDPool()
+        : m_minID(0), m_maxID(0)
+    {}
+
+    IDPool::IDPool(uint64_t minID, uint64_t maxID)
+        : m_minID(minID), m_maxID(maxID)
+    {
+        for (uint64_t i = minID; i <= maxID; ++i)
+        {
+            idQueue.emplace_back(i);
+        }
+    }
+
+    IDPool::~IDPool()
+    {
+        for(auto& id : idList)
+        {
+            releaseID(*id);
+        }
+    }
+
+    ID IDPool::getID()
+    {
+        if (idQueue.empty())
+        {
+            return ID();
+        }
+
+        uint64_t idRaw = idQueue.front();
+        idQueue.pop_front();
+
+        auto id = std::make_shared<uint64_t>(idRaw);
+        idList.emplace_back(id);
+        return ID(id, this);
+    }
+
+    void IDPool::releaseID(uint64_t id)
+    {
+        for (auto it = idList.begin(); it != idList.end(); ++it)
+        {
+            if (**it == id)
+            {
+                idQueue.emplace_back(id);
+                **it = -1;
+                idList.erase(it);
+                break;
+            }
+        }
+
+        sortIDs();
+    }
+
+    void IDPool::releaseID(ID id)
+    {
+        releaseID(id.get());
+    }
+
+    uint64_t IDPool::getMinID() const
+    {
+        return m_minID;
+    }
+
+    uint64_t IDPool::getMaxID() const
+    {
+        return m_maxID;
+    }
+
+    uint64_t IDPool::getAvailableIDs() const
+    {
+        return idQueue.size();
+    }
+
+    void IDPool::sortIDs()
+    {
+        std::sort(idQueue.begin(), idQueue.end());
+    }
+
+    ID::ID()
+        : id(nullptr), pool(nullptr)
+    {
+    }
+
+    ID::ID(std::shared_ptr<uint64_t> id, IDPool* pool)
+        : id(id), pool(pool)
+    {
+    }
+
+    ID::ID(const ID& other)
+        : id(other.id), pool(other.pool)
+    {
+    }
+
+    ID::ID(ID&& other)
+        : id(std::move(other.id)), pool(std::move(other.pool))
+    {
+    }
+
+    ID::~ID()
+    {
+        if (id)
+        {
+            pool->releaseID(*id);
+        }
+    }
+
+    uint64_t ID::get() const
+    {
+        return *id;
+    }
+}
