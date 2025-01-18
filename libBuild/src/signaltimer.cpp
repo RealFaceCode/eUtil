223c2e8123bc116e8f9ec87a46aa5bf124bbc7b5
RealFaceCode <kevin-ernst@xyz.de>
2025-01-18 20:52:34 +0100

    Refactor SignalTimer class by removing EUTIL_API specifiers and adding setSeconds method

--- libBuild/src/signaltimer.cpp
+++ libBuild/src/signaltimer.cpp
@@ -2,34 +2,35 @@
 
 namespace eutil
 {
-    EUTIL_API SignalTimer::SignalTimer() = default;
-
-    EUTIL_API SignalTimer::SignalTimer(double seconds, double secondsActive)
+    SignalTimer::SignalTimer(double seconds, double secondsActive)
         : m_seconds(seconds), m_secondsActive(secondsActive)
     {}
 
-    EUTIL_API SignalTimer::~SignalTimer() = default;
+    void SignalTimer::setSeconds(double seconds)
+    {
+        m_seconds = seconds;
+    }
 
-    EUTIL_API void SignalTimer::start()
+    void SignalTimer::start()
     {
         m_running = true;
         m_start = std::chrono::high_resolution_clock::now();
     }
 
-    EUTIL_API void SignalTimer::stop()
+    void SignalTimer::stop()
     {
         m_running = false;
         m_elapsed = std::chrono::high_resolution_clock::now();
     }
 
-    EUTIL_API void SignalTimer::reset()
+    void SignalTimer::reset()
     {
         m_running = false;
         m_start = std::chrono::high_resolution_clock::now();
         m_elapsed = std::chrono::high_resolution_clock::now();
     }
 
-    EUTIL_API double SignalTimer::elapsed() const
+    double SignalTimer::elapsed() const
     {
         if (m_running)
             return std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - m_start).count();
@@ -37,7 +38,7 @@ namespace eutil
             return std::chrono::duration_cast<std::chrono::duration<double>>(m_elapsed - m_start).count();
     }
 
-    EUTIL_API bool SignalTimer::check()
+    bool SignalTimer::check()
     {
         bool result = elapsed() >= m_seconds;
         if(result && elapsed() >= m_seconds + m_secondsActive)
